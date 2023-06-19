c2fd3c5973b53d708b439d9c7b589a286f86e217
David Jeong <davidjeong221@gmail.com>
2023-06-19 14:34:48 -0400

    e28 part 3

--- packages/e28-assessment-5-lucky-number/lucky-nums/app.py
+++ packages/e28-assessment-5-lucky-number/lucky-nums/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
+import random
+
+API_BASE_URL = "http://numbersapi.com"
 
 app = Flask(__name__)
 
@@ -8,3 +12,45 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+
+def check_for_errors(name, email, year, color):
+    return (not name or not email or
+            not year or year > 2000 or year < 1900 or
+            (color != 'red' and color != 'orange' and
+             color != 'green' and color != 'blue'))
+
+
+def return_error_json(name, email, year, color):
+    errors = {"errors": {}}
+    if not name:
+        errors["errors"]["name"] = "This field is required"
+    if not email:
+        errors["errors"]["email"] = "This field is required"
+    if (color != 'red' and color != 'orange' and
+            color != 'green' and color != 'blue'):
+        errors["errors"]["color"] = 'Invalid value, must be one of: '\
+            'red, green, orange, blue.'
+    if (not year or year > 2000 or year < 1900):
+        errors["errors"]["year"] = 'Invalid value, must be a number '\
+            'between 1900 & 2000.'
+    return jsonify(errors)
+
+
+@app.route("/api/get-lucky-num", methods=["POST"])
+def get_lucky_num():
+    name = request.json['name']
+    email = request.json['email']
+    year = (int(request.json['year']) if request.json['year'].isnumeric()
+            else '')
+    color = request.json['color']
+
+    if check_for_errors(name, email, year, color):
+        return return_error_json(name, email, year, color)
+
+    num = random.randrange(1, 101)
+    num_fact = requests.get(f"{API_BASE_URL}/{num}").text
+    year_fact = requests.get(f"{API_BASE_URL}/{year}/year").text
+    data = {"num": {"fact": num_fact, "num": num},
+            "year": {"fact": year_fact, "year": year}}
+    return jsonify(data)
--- packages/e28-assessment-5-lucky-number/lucky-nums/static/lucky.js
+++ packages/e28-assessment-5-lucky-number/lucky-nums/static/lucky.js
@@ -1,11 +1,40 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
+    evt.preventDefault();
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const year = document.querySelector('#year');
+    const color = document.querySelector('#color');
+    const data = {
+        name: name.value,
+        email: email.value,
+        year: year.value,
+        color: color.value
+    };
+    resp = await axios.post('/api/get-lucky-num', data);
+    handleResponse(resp);
+    document.querySelector('#lucky-form').reset();
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
+    const results = document.querySelector('#lucky-results');
+    results.innerHTML = '';
+    const messages = document.querySelectorAll('b');
+    for (let message of messages) {
+        message.innerText = '';
+    }
+    if (Object.keys(resp.data)[0] == 'errors') {
+        for (let error in resp.data.errors) {
+            const errorMsg = document.querySelector(`#${error}-err`);
+            errorMsg.innerHTML = `${resp.data.errors[error]}`;
+        }
+    } else {
+        results.innerHTML = `<p>Your lucky number is ${resp.data.num.num} (${resp.data.num.fact})</p>
+        <p>Your birth year (${resp.data.year.year}) fact is ${resp.data.year.fact}</p>`;
+    }
 }
 
 
