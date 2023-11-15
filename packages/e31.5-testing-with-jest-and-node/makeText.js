c527d8a0d8a1f2ecb74ebee7b461fbadd0461621
David Jeong <davidjeong221@gmail.com>
2023-11-14 19:50:38 -0500

    e31.5

--- packages/e31.5-testing-with-jest-and-node/makeText.js
+++ packages/e31.5-testing-with-jest-and-node/makeText.js
@@ -1,2 +1,41 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const axios = require('axios');
+const { MarkovMachine } = require('./markov');
+
+function read(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log('ERROR:', err);
+            process.kill(1);
+        }
+        let mm = new MarkovMachine(data);
+        console.log(mm.makeText());
+    });
+}
+
+async function readURL(path) {
+    try {
+        const res = await axios.get(path);
+        let mm = new MarkovMachine(res.data);
+        console.log(mm.makeText());
+    } catch (err) {
+        console.log("ERROR:", err);
+        process.kill(1);
+    }
+}
+
+function checkArgs(args) {
+    if (args[2] == 'file') {
+        read(args[3]);
+    } else if (args[2] == 'url') {
+        readURL(args[3]);
+    } else {
+        console.log('ERROR: Unknown input');
+        process.kill(1);
+    }
+}
+
+checkArgs(process.argv);
+
