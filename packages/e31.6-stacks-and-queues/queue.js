cffe0e62ba6a8e9dfe35c7d72ef5e58b69613626
David Jeong <davidjeong221@gmail.com>
2023-11-16 20:34:48 -0500

    e31.6

--- packages/e31.6-stacks-and-queues/queue.js
+++ packages/e31.6-stacks-and-queues/queue.js
@@ -1,46 +1,65 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
 
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
-  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+    /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+    enqueue(val) {
+        const node = new Node(val);
+        if (!this.first) {
+            this.first = node;
+            this.last = node;
+        } else {
+            this.last.next = node;
+            this.last = this.last.next;
+        }
+        this.size++;
+        return undefined;
+    }
 
-  }
+    /** dequeue(): remove the node from the start of the queue
+     * and return its value. Should throw an error if the queue is empty. */
 
-  /** dequeue(): remove the node from the start of the queue
-   * and return its value. Should throw an error if the queue is empty. */
+    dequeue() {
+        if (!this.first) {
+            throw new Error('Queue is empty, cannot dequeue!');
+        }
+        try {
+            const node = this.first;
+            this.first = this.first.next;
+            this.size--;
+            return node.val;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-  dequeue() {
+    /** peek(): return the value of the first node in the queue. */
 
-  }
+    peek() {
+        return this.first.val;
+    }
 
-  /** peek(): return the value of the first node in the queue. */
+    /** isEmpty(): return true if the queue is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the queue is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+    isEmpty() {
+        return this.first == null;
+    }
 }
 
 module.exports = Queue;
