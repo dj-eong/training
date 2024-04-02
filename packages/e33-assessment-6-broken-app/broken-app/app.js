c4197d1c3d97e0217e2b818cd8189dc2cf427472
David Jeong <davidjeong221@gmail.com>
2024-04-02 13:50:24 -0400

    e33

--- packages/e33-assessment-6-broken-app/broken-app/app.js
+++ packages/e33-assessment-6-broken-app/broken-app/app.js
@@ -1,18 +1,33 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+const app = express();
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
-  }
+app.use(express.json());
+
+app.get('/', async function (req, res, next) {
+	try {
+		const results = await getDevInfo(req);
+		res.json(results);
+	} catch (e) {
+		next(e);
+	}
 });
 
-app.listen(3000);
+async function getDevInfo(req) {
+	const results = [];
+	for (developer of req.body.developers) {
+		const response = await axios.get(`https://api.github.com/users/${developer}`);
+		results.push({ name: response.data.name, bio: response.data.bio });
+	}
+	return results;
+}
+
+// Error handler
+app.use(function (err, req, res, next) {
+	return res.status(+err.message.slice(err.message.length - 3)).json({ error: err });
+});
+
+app.listen(3000, function () {
+	console.log("Server is listening on port 3000");
+});
--- packages/e33-assessment-6-broken-app/broken-app/issues.md
+++ packages/e33-assessment-6-broken-app/broken-app/issues.md
@@ -1,1 +1,7 @@
 # Broken App Issues
+
+- No package.json file. The node package manager was never initialized, and the required technologies (express & axios) were never installed. I created a package.json file with the required dependencies indicated.
+- No error handling. I added an error handler that would return in json format the proper error code and message.
+- API request logic in node route. I refactored the code, moving the external API request logic into a helper function for better readability.
+- Incorrect and outdated syntax. The `/` route was coded as a POST request when it should have been a GET request. I also updated all variable declarations of `var` and even `let` to `const` whenever possible.
+- Inefficiency. The original logic used unnecessary space and runtime, looping through the list of devs twice and creating two arrays to store dev info. I improved the code to iterate through the dev list once and store only the necessary info into one array.
